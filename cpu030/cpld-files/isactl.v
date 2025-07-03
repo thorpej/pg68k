01c0eb0eade486192c44630a3442a4e6d92bb3e6
Jason Thorpe <thorpej@me.com>
2025-07-03 12:44:40 -0700

    Add a comprehensive ISA controller.

---
+++ cpu030/cpld-files/isactl.v
@@ -0,0 +1,337 @@
+/*
+ * Copyright (c) 2025 Jason R. Thorpe.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Playground 68030 ISA bus controller.
+ *
+ * This contains the glue logic to convert the 68k bus signalling to
+ * ISA bus signalling, generate the timing, and select the on-board
+ * ISA (and ISA-like) peripherals.
+ *
+ * TODO:
+ * - Add timings for ATA PIO modes other than mode 0.
+ * - Optimize the cycle timings to squeeze out clock cycles where possible.
+ * - Add a generic ISA bus interface that defaults to the slow timings?
+ *   Right now, an unknown device will generate a bus error.
+ */
+
+module isactl(
+	input wire nRST,
+	input wire CPU_CLK,
+
+	input wire [9:0] ADDR,
+	inout wire [7:0] DATA,
+
+	input wire nISASEL,
+	input wire nAS,
+	input wire nDS,
+	input wire RnW,
+	input wire [1:0] SIZ,
+
+	input wire ISA_RDY,
+	input wire nISA_IO16,
+	input wire nISA_MEM16,
+
+	output wire BERR,		/* external open-drain inv */
+	output wire [1:0] DSACK,	/* external open-drain inv */
+
+	output wire ISA_AEN,
+	output wire nISA_IORD,
+	output wire nISA_IOWR,
+
+	output wire nDUARTSEL,
+	output wire nTMRSEL,
+	output wire nATASEL,
+	output wire nATAAUXSEL
+);
+
+/* /AS will have been asserted by definition. */
+assign ISA_AEN = ~nISASEL;
+
+/*
+ * Put devices in ISA-like locations:
+ *
+ *	DUART     -> 0x3f8 (0), 0x2f8 (1) (DUART checks A8) - 8 bytes
+ *	Timer     -> 0x040 - 8 bytes
+ *	ATA disk  -> 0x1f0, 0x3f6 - 8 bytes, 2 bytes
+ *	RTL8019AS -> 0x300 - 32 bytes
+ *
+ * N.B. the RTL8019AS fully decodes its own address; we do not need to
+ * provide a chip select output for it, but we do need to know when it
+ * is selected.
+ */
+wire nETHSEL;
+wire nPIOMODESEL;
+
+localparam DEV_DUART		= 10'b1x11111xxx;
+localparam DEV_TIMER		= 10'b0001000xxx;
+localparam DEV_ATA		= 10'b0111110xxx;
+localparam DEV_ATA_AUX		= 10'b1111110110;
+localparam DEV_ATA_PMODE	= 10'b1111110111;
+localparam DEV_ETH		= 10'b11000xxxxx;
+
+localparam SEL_dc	 = 6'bxxxxxx;
+localparam SEL_NONE	 = 6'b111111;
+localparam SEL_DUART	 = 6'b011111;
+localparam SEL_TIMER	 = 6'b101111;
+localparam SEL_ATA	 = 6'b110111;
+localparam SEL_ATA_AUX	 = 6'b111011;
+localparam SEL_ATA_PMODE = 6'b111101;
+localparam SEL_ETH	 = 6'b111110;
+
+reg [5:0] DevSelectOutputs;
+always @(*) begin
+	casex ({nISASEL, ADDR})
+	{1'b0, DEV_DUART}:     DevSelectOutputs = SEL_DUART;
+	{1'b0, DEV_TIMER}:     DevSelectOutputs = SEL_TIMER;
+	{1'b0, DEV_ATA}:       DevSelectOutputs = SEL_ATA;
+	{1'b0, DEV_ATA_AUX}:   DevSelectOutputs = SEL_ATA_AUX;
+	{1'b0, DEV_ATA_PMODE}: DevSelectOutputs = SEL_ATA_PMODE;
+	{1'b0, DEV_ETH}:       DevSelectOutputs = SEL_ETH;
+	default:               DevSelectOutputs = SEL_NONE;
+	endcase
+end
+assign {nDUARTSEL, nTMRSEL, nATASEL, nATAAUXSEL, nPIOMODESEL, nETHSEL} =
+    DevSelectOutputs;
+
+reg bus_error;
+assign BERR = bus_error;
+
+/* This register holds the current PIO mode for the ATA interface. */
+reg [1:0] PIO_mode;
+
+reg enable_data_out;
+reg [7:0] data_out;
+always @(*) begin
+	case (DevSelectOutputs)
+	SEL_ATA_PMODE:	data_out = {6'b0, PIO_mode};
+	default:	data_out = 8'hFF;
+	endcase
+end
+
+assign DATA = enable_data_out ? data_out : 8'bzzzzzzzz;
+
+localparam IO_STROBE_NONE  = 2'b00;
+localparam IO_STROBE_READ  = 2'b10;
+localparam IO_STROBE_WRITE = 2'b01;
+wire [1:0] io_strobe_type = {RnW, ~RnW};
+
+reg [1:0] io_strobe;
+assign {nISA_IORD, nISA_IOWR} = ~io_strobe;
+
+reg [1:0] dsack;
+assign DSACK = dsack;
+
+localparam Idle		= 4'd0;
+localparam ATA_t1_0_8	= 4'd1;
+localparam ATA_t1_0_16	= 4'd2;
+localparam ATA_t1_1_8	= 4'd3;
+localparam ATA_t1_1_16	= 4'd4;
+localparam ISA_rw_3w	= 4'd5;
+localparam ISA_rw_2w	= 4'd6;
+localparam ISA_w8	= 4'd7;
+localparam ISA_w7	= 4'd8;
+localparam ISA_w6	= 4'd9;
+localparam ISA_w5	= 4'd10;
+localparam ISA_w4	= 4'd11;
+localparam ISA_w3	= 4'd12;
+localparam ISA_w2	= 4'd13;
+localparam ISA_w1	= 4'd14;
+localparam TermWait	= 4'd15;
+
+wire [5:0] Cycle = {nAS, nDS, RnW, SIZ, ADDR[0]};
+localparam NONE		= 6'b1xxxxx;
+localparam STARTWRITE	= 6'b010xxx;
+localparam READ8	= 6'b00101x;
+localparam WRITE8	= 6'b00001x;
+localparam READ16	= 6'b001100;
+localparam WRITE16	= 6'b000100;
+localparam ANY8		= 6'b00x01x;
+localparam ANY16	= 6'b00x100;
+
+reg [3:0] state;
+always @(posedge CPU_CLK, negedge nRST) begin
+	if (~nRST) begin
+		enable_data_out <= 1'b0;
+		io_strobe <= IO_STROBE_NONE;
+		bus_error <= 1'b0;
+		dsack <= 2'b00;
+		PIO_mode <= 2'd0;
+		state <= Idle;
+	end
+	else begin
+		/* NOTE: Each state represents 40ns. */
+		case (state)
+		Idle: begin
+			/*
+			 * If we get any decode hit here, we know that
+			 * the chip select for the device has already
+			 * been asserted by combinatorial logic.  For
+			 * read cycles, /AS and /DS are asserted at the
+			 * same time, whereas for write cycles, /DS is
+			 * assrted one CPU clock cycle later than /AS.
+			 *
+			 * This means that the number of slots needed
+			 * for address setup times might be different
+			 * for reads vs. writes.
+			 */
+			casex ({Cycle, DevSelectOutputs})
+			{ANY8, SEL_ATA}: begin
+				state <= ATA_t1_0_8;
+			end
+
+			{ANY8, SEL_ATA_AUX}: begin
+				state <= ATA_t1_0_8;
+			end
+
+			{ANY16, SEL_ATA}: begin
+				state <= ATA_t1_0_16;
+			end
+
+			{READ8, SEL_ATA_PMODE}: begin
+				enable_data_out = 1'b1;
+				dsack <= SIZ;
+				state <= TermWait;
+			end
+
+			{WRITE8, SEL_ATA_PMODE}: begin
+				PIO_mode <= DATA[1:0];
+				dsack <= SIZ;
+				state <= TermWait;
+			end
+
+			{ANY8, SEL_TIMER}: begin
+				state <= ISA_rw_3w;
+			end
+
+			{ANY8, SEL_DUART}: begin
+				io_strobe <= io_strobe_type;
+				state <= ISA_w1;  /* maybe 1 clock faster? */
+			end
+
+			{ANY8, SEL_ETH}: begin
+				state <= ISA_rw_2w;
+			end
+
+			{ANY16, SEL_ETH}: begin
+				state <= ISA_rw_2w;
+			end
+
+			{STARTWRITE, SEL_dc}: begin
+				state <= Idle;
+			end
+
+			{NONE, SEL_dc}: begin
+				state <= Idle;
+			end
+
+			default: begin
+				bus_error <= 1'b1;
+				state <= TermWait;
+			end
+			endcase
+		end
+
+		/*
+		 * ATA state machine matches timings for PIO mode 0
+		 */
+		ATA_t1_0_8: begin
+			state <= ATA_t1_1_8;
+		end
+
+		ATA_t1_0_16: begin
+			state <= ATA_t1_1_16;
+		end
+
+		ATA_t1_1_8: begin
+			io_strobe <= io_strobe_type;
+			state <= ISA_w8;
+		end
+
+		ATA_t1_1_16: begin
+			io_strobe <= io_strobe_type;
+			state <= ISA_w5;
+		end
+
+		ISA_rw_3w: begin
+			io_strobe <= io_strobe_type;
+			state <= ISA_w3;
+		end
+
+		ISA_rw_2w: begin
+			io_strobe <= io_strobe_type;
+			state <= ISA_w2;
+		end
+
+		ISA_w8: begin
+			state <= ISA_w7;
+		end
+
+		ISA_w7: begin
+			state <= ISA_w6;
+		end
+
+		ISA_w6: begin
+			state <= ISA_w5;
+		end
+
+		ISA_w5: begin
+			state <= ISA_w4;
+		end
+
+		ISA_w4: begin
+			state <= ISA_w3;
+		end
+
+		ISA_w3: begin
+			state <= ISA_w2;
+		end
+
+		ISA_w2: begin
+			state <= ISA_w1;
+		end
+
+		ISA_w1: begin
+			if (ISA_RDY) begin
+				dsack <= SIZ;
+				state <= TermWait;
+			end
+		end
+
+		TermWait: begin
+			if (nAS) begin
+				enable_data_out = 1'b0;
+				io_strobe <= IO_STROBE_NONE;
+				bus_error <= 1'b0;
+				dsack <= 2'b00;
+				state <= Idle;
+			end
+		end
+		endcase
+	end
+end
+
+endmodule
---
+++ cpu030/cpld-files/isactl_tb.v
@@ -0,0 +1,614 @@
+/*
+ * Copyright (c) 2025 Jason R. Thorpe.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ *    Initial
+ *       |
+ *       |
+ *       v
+ *        ___     ___     ___
+ * 50MHz |   |   |   |   |   |   |     DRAM
+ *       |   |___|   |___|   |___|
+ *       ^       ^
+ *       |  20ns |
+ *
+ *        _______         _______
+ * 25MHz |       |       |             68030
+ *       |       |_______|
+ *       ^               ^
+ *       |      40ns     |
+ *       A   B   C   D   E   F   G
+ *
+ * Signal transition in the 68030 can happen around B, D, and F.
+ */
+`timescale 10ns/1ns
+
+module tb();
+
+localparam CLK30	= 4;	/* full 030 clock */
+localparam HCLK30	= 2;	/* half 030 clock */
+localparam QCLK30	= 1;	/* quarter 030 clock */
+
+reg n_rst;
+reg clk;
+reg n_as;
+reg n_ds;
+reg rnw;
+
+reg [1:0] siz;
+reg [9:0] addr;
+reg [7:0] data;
+
+wire [7:0] data_out;
+assign data_out = data;
+
+reg n_isasel;
+
+reg isa_rdy;
+reg n_isa_io16;
+reg n_isa_mem16;
+
+wire berr;
+wire [1:0] dsack;
+
+wire isa_aen;
+wire n_isa_iord;
+wire n_iso_iowr;
+
+wire n_duartsel;
+wire n_tmrsel;
+wire n_atasel;
+wire n_ataauxsel;
+
+wire cycle_terminated = berr | dsack[0] | dsack[1];
+
+	/* Instantiate the device under test. */
+	isa_glue dut (
+		.nRST(n_rst),
+		.CPU_CLK(clk),
+		.nAS(n_as),
+		.nDS(n_ds),
+		.nISASEL(n_isasel),
+		.RnW(rnw),
+		.SIZ(siz),
+		.ADDR(addr),
+		.DATA(data_out),
+		.ISA_RDY(isa_rdy),
+		.nISA_IO16(n_isa_io16),
+		.nISA_MEM16(n_isa_mem16),
+		.DSACK(dsack),
+		.BERR(berr),
+		.ISA_AEN(isa_aen),
+		.nISA_IORD(n_isa_iord),
+		.nISA_IOWR(n_isa_iowr),
+		.nDUARTSEL(n_duartsel),
+		.nTMRSEL(n_tmrsel),
+		.nATASEL(n_atasel),
+		.nATAAUXSEL(n_ataauxsel)
+	);
+
+	initial begin
+		$dumpfile("isaglue_tb.vcd");
+		$dumpvars;
+		clk = 1;	/* 1 cycle is rising-edge-to-rising-edge */
+		n_rst = 1;
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		rnw = 1;
+
+		isa_rdy = 1;	/* no device-inserted wait states for now */
+
+		siz = 2'd0;
+		addr = 10'h000;
+	end
+
+	always #2 clk = ~clk;	/* clk == cpu_clk */
+
+	initial begin
+		n_rst = 0;
+		#(CLK30);	/* reset for 1 68030 clock */
+		n_rst = 1;
+
+		/*
+		 * Do a DUART read.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h3f8;
+		rnw = 1;
+		siz = 2'b01;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS and /DS asserted.
+		 */
+		n_as = 0;
+		n_ds = 0;
+		n_isasel = 0;
+		#(HCLK30);
+
+		/*
+		 * ** S2 **
+		 * Unrelated control signals happen.
+		 */
+		#(HCLK30);
+
+		/*
+		 * ** S3 **
+		 * Wait for cycle termination.
+		 */
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+		/*
+		 * Do a DUART write.  To the other UART.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h2f8;
+		rnw = 0;
+		siz = 2'b01;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS asserted.
+		 */
+		n_as = 0;
+		n_isasel = 0;
+		#(HCLK30)
+
+		/*
+		 * ** S2 **
+		 * Data placed onto data bus.
+		 */
+		#(HCLK30)
+
+		/*
+		 * ** S3 **
+		 * /DS asserted.  Wait for cycle termination.
+		 */
+		n_ds = 0;
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+		/*
+		 * Do a 16-bit Ethernet data write.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h310;
+		rnw = 0;
+		siz = 2'b10;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS asserted.
+		 */
+		n_as = 0;
+		n_isasel = 0;
+		#(HCLK30)
+
+		/*
+		 * ** S2 **
+		 * Data placed onto data bus.
+		 */
+		#(HCLK30)
+
+		/*
+		 * ** S3 **
+		 * /DS asserted.  Wait for cycle termination.
+		 */
+		n_ds = 0;
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+		/*
+		 * Do a TIMER read.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h040;
+		rnw = 1;
+		siz = 2'b01;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS and /DS asserted.
+		 */
+		n_as = 0;
+		n_ds = 0;
+		n_isasel = 0;
+		#(HCLK30);
+
+		/*
+		 * ** S2 **
+		 * Unrelated control signals happen.
+		 */
+		#(HCLK30);
+
+		/*
+		 * ** S3 **
+		 * Wait for cycle termination.
+		 */
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+		/*
+		 * Do a TIMER write.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h040;
+		rnw = 0;
+		siz = 2'b01;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS asserted.
+		 */
+		n_as = 0;
+		n_isasel = 0;
+		#(HCLK30)
+
+		/*
+		 * ** S2 **
+		 * Data placed onto data bus.
+		 */
+		#(HCLK30)
+
+		/*
+		 * ** S3 **
+		 * /DS asserted.  Wait for cycle termination.
+		 */
+		n_ds = 0;
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+		/*
+		 * Do an 8-bit ATA AUX write.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h3f6;
+		rnw = 0;
+		siz = 2'b01;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS asserted.
+		 */
+		n_as = 0;
+		n_isasel = 0;
+		#(HCLK30)
+
+		/*
+		 * ** S2 **
+		 * Data placed onto data bus.
+		 */
+		#(HCLK30)
+
+		/*
+		 * ** S3 **
+		 * /DS asserted.  Wait for cycle termination.
+		 */
+		n_ds = 0;
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+		/*
+		 * 16-bit ATA data read.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h1f0;
+		rnw = 1;
+		siz = 2'b10;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS and /DS asserted.
+		 */
+		n_as = 0;
+		n_ds = 0;
+		n_isasel = 0;
+		#(HCLK30);
+
+		/*
+		 * ** S2 **
+		 * Unrelated control signals happen.
+		 */
+		#(HCLK30);
+
+		/*
+		 * ** S3 **
+		 * Wait for cycle termination.
+		 */
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+		/*
+		 * Do a 16-bit unaligned write to trigger a bus error.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h311;
+		rnw = 0;
+		siz = 2'b10;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS asserted.
+		 */
+		n_as = 0;
+		n_isasel = 0;
+		#(HCLK30)
+
+		/*
+		 * ** S2 **
+		 * Data placed onto data bus.
+		 */
+		#(HCLK30)
+
+		/*
+		 * ** S3 **
+		 * /DS asserted.  Wait for cycle termination.
+		 */
+		n_ds = 0;
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+		/*
+		 * Write 2 to the PIO_mode register.
+		 */
+
+		/*
+		 * ** S0 **
+		 * - Address, RnW, and SIZ become valid.
+		 */
+		addr = 10'h3f7;
+		rnw = 0;
+		siz = 2'b01;
+		#(HCLK30)
+
+		/*
+		 * ** S1 **
+		 * /AS asserted.
+		 */
+		n_as = 0;
+		n_isasel = 0;
+		#(HCLK30)
+
+		/*
+		 * ** S2 **
+		 * Data placed onto data bus.
+		 */
+		data = 2;
+		#(HCLK30)
+
+		/*
+		 * ** S3 **
+		 * /DS asserted.  Wait for cycle termination.
+		 */
+		n_ds = 0;
+		while (~cycle_terminated) begin
+			#CLK30;
+		end
+		#HCLK30;
+
+		/*
+		 * ** S4 **
+		 * Wait for 1/2 clock.
+		 */
+		#HCLK30;
+
+		/*
+		 * ** S5 **
+		 * De-assert /AS and /DS.
+		 */
+		n_as = 1;
+		n_ds = 1;
+		n_isasel = 1;
+		#HCLK30;
+
+
+
+
+		/* 4 clocks to observe idle. */
+		#(CLK30 * 4);
+
+		$finish;
+	end
+
+endmodule

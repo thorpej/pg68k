de46b56ec929ecb07eb532c29acb4a15f593d60f
Jason Thorpe <thorpej@me.com>
2025-07-17 08:14:51 -0700

    NetBSD's genassym for generating assembly symbols for C structures.

--- system_rom/sysrom.mk
+++ system_rom/sysrom.mk
@@ -2,7 +2,7 @@
 # Common Makefile infrastructure for the pg68k system ROM.
 #
 
-CPPFLAGS=	-I.. -DCONFIG_MACH_$(MACH_TYPE)
+CPPFLAGS=	-I. -I.. -DCONFIG_MACH_$(MACH_TYPE)
 CFLAGS=		-Os
 ASFLAGS=
 
@@ -11,14 +11,25 @@ CFLAGS+=	-mcpu=$(MACH_CPU)
 ASFLAGS+=	-mcpu=$(MACH_CPU)
 .endif
 
+GENASSYM_FLAGS=${CFLAGS:N-Wa,*:N-fstack-usage*} ${CPPFLAGS}
+
 .PATH: ..
+
+assym.h: genassym.sh genassym.cf
+	cat ../genassym.cf | \
+	    sh ../genassym.sh -- ${CC} ${GENASSYM_FLAGS} > assym.h.tmp && \
+	mv -f assym.h.tmp assym.h
 
 SYSLIBOBJS=	memcmp.o memcpy.o memset.o subr_prf.o
 
 M68KOBJS=	start.o setjmp.o trap_stubs.o trap.o
 OBJS=		$(SYSLIBOBJS) main.o uart.o console.o
 
+CLEANFILES=	assym.h $(MACH_CLEANFILES)
+
+${M68KOBJS}:	assym.h
+
 all: $(MACH_IMGS)
 
 clean:
-	-rm -f $(M68KOBJS) $(OBJS) $(MACH_PROG) $(MACH_IMGS) $(MACH_CLEANFILES)
+	-rm -f $(M68KOBJS) $(OBJS) $(MACH_PROG) $(MACH_IMGS) $(CLEANFILES)

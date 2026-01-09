d483dddc43728b14c366535c9cada49c53a7bd59
Jason Thorpe <thorpej@me.com>
2026-01-09 11:54:37 -0800

    Scripts for making ioctl010 and memctl010.

---
+++ cpu010/cpld-files/make_memctl010.cmd
@@ -0,0 +1,49 @@
+@echo off
+
+set VAR_FITTERDIR=C:\ATMEL_PLS_Tools\Prochip\pldfit
+set VAR_NAME="memctl010"
+
+set VAR_DEVICE=ATF1504AS
+set VAR_PACKAGE=PLCC44
+set VAR_SPEED=10
+set VAR_FITTER=fit%VAR_DEVICE:~3,4%.exe
+
+if not exist "%VAR_FITTERDIR%\atmel.std" (
+	echo Unable to locate Atmel fitters
+	exit /b 1
+)
+
+if exist "%VAR_NAME%.fit" del /f /q "%VAR_NAME%.fit">nul
+
+echo %VAR_FITTERDIR%\%VAR_FITTER% ^
+-i "%VAR_NAME%.edif" ^
+-ifmt edif ^
+-o "%VAR_NAME%.jed" ^
+-lib "%VAR_FITTERDIR%\aprim.lib" ^
+-tech "%VAR_DEVICE%" ^
+-device "%VAR_PACKAGE%" ^
+-tpd "%VAR_SPEED%"
+
+%VAR_FITTERDIR%\%VAR_FITTER% ^
+-i "%VAR_NAME%.edif" ^
+-ifmt edif ^
+-o "%VAR_NAME%.jed" ^
+-lib "%VAR_FITTERDIR%\aprim.lib" ^
+-tech "%VAR_DEVICE%" ^
+-device "%VAR_PACKAGE%" ^
+-tpd "%VAR_SPEED%"
+
+findstr /R /C:"^    " "%VAR_NAME%.fit"
+echo.
+
+echo Logic Array Block       Macro Cells     I/O Pins        Foldbacks       TotalPT         FanIN           Cascades
+findstr /R /C:"^[A-H]:" "%VAR_NAME%.fit"
+echo.
+
+findstr /R /C:"^Total" "%VAR_NAME%.fit"
+echo.
+
+findstr /R /C:"^$Device" "%VAR_NAME%.fit"
+echo.
+
+exit /b 0

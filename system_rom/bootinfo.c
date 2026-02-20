9c343ce4d7e6e41c8e5663d54aa0963fdf55790c
Jason Thorpe <thorpej@me.com>
2026-02-20 05:54:31 -0500

    Minor cleanup.

---
+++ system_rom/bootinfo.c
@@ -0,0 +1,277 @@
+/*              
+ * Copyright (c) 2026 Jason R. Thorpe.
+ * All rights reserved. 
+ *              
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:             
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *                      
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE. 
+ */             
+
+#include "config.h"
+#include "syslib.h"
+#include "memory.h"
+#include "bootinfo.h"
+
+#ifdef CONFIG_DEVICETREE
+#include "libfdt.h"
+#endif
+
+static struct bi_record *
+_bootinfo_set_size(struct bi_record *bi, size_t sz, bool wr)
+{
+	uintptr_t addr = (uintptr_t)bi;
+
+	sz = (sz + sizeof(*bi) + 3) & ~3;
+	if (wr) {
+		bi->bi_size = sz;
+	}
+	return (struct bi_record *)(addr + sz);
+}
+
+static struct bi_record *
+_bootinfo_set_u32(struct bi_record *bi, uint16_t tag, uint32_t val, bool wr)
+{
+	if (wr) {
+		*(uint32_t *)bootinfo_dataptr(bi) = val;
+	}
+	return _bootinfo_set_size(bi, sizeof(val), wr);
+}
+
+struct bi_record *
+bootinfo_set_u32(struct bi_record *bi, uint16_t tag, uint32_t val)
+{
+	return _bootinfo_set_u32(bi, tag, val, true);
+}
+
+static struct bi_record *
+_bootinfo_set_mem_info(struct bi_record *bi, uint16_t tag,
+    uint32_t addr, uint32_t size, bool wr)
+{
+	struct bi_mem_info *m = bootinfo_dataptr(bi);
+
+	if (wr) {
+		m->mem_addr = addr;
+		m->mem_size = size;
+	}
+
+	return _bootinfo_set_size(bi, sizeof(*m), wr);
+}
+
+struct bi_record *
+bootinfo_set_mem_info(struct bi_record *bi, uint16_t tag,
+    uint32_t addr, uint32_t size)
+{
+	return _bootinfo_set_mem_info(bi, tag, addr, size, true);
+}
+
+static struct bi_record *
+_bootinfo_set_data(struct bi_record *bi, uint16_t tag,
+    const void *data, size_t len, bool wr)
+{
+	struct bi_data *d = bootinfo_dataptr(bi);
+
+	if (wr) {
+		d->data_length = (uint16_t)len;
+		memcpy(&d->data_bytes[0], data, d->data_length);
+	}
+
+	return _bootinfo_set_size(bi, sizeof(*d) + (uint16_t)len, wr);
+}
+
+struct bi_record *
+bootinfo_set_data(struct bi_record *bi, uint16_t tag,
+    const void *data, size_t len)
+{
+	return _bootinfo_set_data(bi, tag, data, len, true);
+}
+
+static struct bi_record *
+_bootinfo_set_string(struct bi_record *bi, uint16_t tag,
+    const char *str, bool wr)
+{
+	return _bootinfo_set_data(bi, tag, str, strlen(str) + 1, wr);
+}
+
+struct bi_record *
+bootinfo_set_string(struct bi_record *bi, uint16_t tag,
+    const char *str)
+{
+	return _bootinfo_set_string(bi, tag, str, true);
+}
+
+#if defined(CONFIG_MC68010)
+#define	BI_CPU_value	BI_CPU_68010
+#elif defined(CONFIG_MC68020)
+#define	BI_CPU_value	BI_CPU_68020
+#elif defined(CONFIG_MC68030)
+#define	BI_CPU_value	BI_CPU_68030
+#elif defined(CONFIG_MC68040)
+#define	BI_CPU_value	BI_CPU_68040
+#elif defined(CONFIG_MC68060)
+#define	BI_CPU_value	BI_CPU_68060
+#endif
+
+#if defined(CONFIG_MC68881)
+#define	BI_FPU_value	BI_FPU_68881
+#elif defined(CONFIG_MC68882)
+#define	BI_FPU_value	BI_FPU_68882
+#elif defined(CONFIG_MC68040) && !defined(CONFIG_MC68LC040)
+#define	BI_FPU_value	BI_FPU_68040
+#elif defined(CONFIG_MC68060) && !defined(CONFIG_MC68LC060)
+#define	BI_FPU_value	BI_FPU_68060
+#endif
+
+#if defined(CONFIG_MC68010)
+#define	BI_MMU_value	BI_MMU_PG010
+#elif defined(CONFIG_MC68020)
+#error need a 68020 case
+#elif defined(CONFIG_MC68030)
+#define	BI_MMU_value	BI_MMU_68030
+#elif defined(CONFIG_MC68040)
+#define	BI_MMU_value	BI_MMU_68040
+#elif defined(CONFIG_MC68060)
+#define	BI_MMU_value	BI_MMU_68060
+#endif
+
+static struct bi_record *
+_bootinfo_populate(struct bi_record *bi, bool wr)
+{
+	struct bi_record *next_bi;
+	int i;
+
+	bi = _bootinfo_set_u32(bi, BI_MACHTYPE, BI_MACH_PG68K, wr);
+	bi = _bootinfo_set_u32(bi, BI_CPUTYPE, BI_CPU_value, wr);
+#ifdef BI_FPU_value
+	/* XXX should also probe for FPU type. */
+	bi = _bootinfo_set_u32(bi, BI_FPUTYPE, BI_FPU_value, wr);
+#endif
+#ifdef BI_MMU_value
+	bi = _bootinfo_set_u32(bi, BI_MMUTYPE, BI_MMU_value, wr);
+#endif
+
+	for (next_bi = NULL, i = 0; i < memory_bank_count; i++) {
+		if (memory_banks[i].size == 0) {
+			continue;
+		}
+
+		/*
+		 * If next_bi != NULL, then bi == the last MEMCHUNK record
+		 * we set.  See if we can merge with the last one in that
+		 * case.
+		 */
+		if (next_bi != NULL) {
+			struct bi_mem_info *m = bootinfo_dataptr(bi);
+			if (m->mem_addr + m->mem_size ==
+			    memory_banks[i].start) {
+				/* merge them! */
+				m->mem_size += memory_banks[i].size;
+				continue;
+			}
+			/* can't merge, add a new record. */
+			bi = next_bi;
+		}
+		next_bi = _bootinfo_set_mem_info(bi, BI_MEMCHUNK,
+		    memory_banks[i].start, memory_banks[i].size, wr);
+	}
+	if (next_bi != NULL) {
+		bi = next_bi;
+	}
+
+#ifdef CONFIG_DEVICETREE
+	void *fdt = get_fdt();
+	int off = fdt_path_offset(fdt, "/");
+	const char *str = fdt_getprop(fdt, off, "compatible", NULL);
+	if (str != NULL) {
+		bi = _bootinfo_set_string(bi, BI_PG68K_PLATFORM, str, wr);
+	}
+	bi = _bootinfo_set_data(bi, BI_PG68K_FDT, fdt, fdt_totalsize(fdt), wr);
+#else
+	bi = _bootinfo_set_string(bi, BI_PG68K_PLATFORM,
+	    CONFIG_MACHINE_STRING, wr);
+#endif /* CONFIG_DEVICETREE */
+
+	/* ELF syms are patched up later. */
+	bi = _bootinfo_set_mem_info(bi, BI_PG68K_ELF_SYMS, 0, 0, wr);
+
+	return bi;
+}
+
+size_t
+bootinfo_size(void)
+{
+	return (size_t)_bootinfo_populate(NULL, false);
+}
+
+void
+bootinfo_populate(void *vbi)
+{
+	(void)_bootinfo_populate(vbi, true);
+}
+
+struct bi_record *
+bootinfo_next(struct bi_record *bi)
+{
+	if (bi->bi_tag == BI_LAST) {
+		return NULL;
+	}
+	return (struct bi_record *)(((uintptr_t)bi) + bi->bi_size);
+}
+
+void
+bootinfo_enumerate(void *vbi, bool (*cb)(struct bi_record *, void *),
+    void *ctx)
+{
+	struct bi_record *bi = vbi;
+
+	for (; bi != NULL; bi = bootinfo_next(bi)) {
+		if ((*cb)(bi, ctx) == false) {
+			break;
+		}
+	}
+}
+
+struct bootinfo_find_ctx {
+	uint32_t tag;
+	struct bi_record *result;
+};
+
+static bool
+bootinfo_find_cb(struct bi_record *bi, void *v)
+{
+	struct bootinfo_find_ctx *ctx = v;
+
+	if (bi->bi_tag == ctx->tag) {
+		ctx->result = bi;
+		return false;
+	}
+
+	return true;
+}
+
+struct bi_record *
+bootinfo_find(void *vbi, uint32_t tag)
+{
+	struct bootinfo_find_ctx ctx = {
+		.tag = tag,
+	};
+
+	bootinfo_enumerate(vbi, bootinfo_find_cb, &ctx);
+	return ctx.result;
+}
---
+++ system_rom/bootinfo.h
@@ -0,0 +1,181 @@
+/*	NetBSD: bootinfo.h,v 1.8 2025/11/14 00:44:14 thorpej Exp	*/
+
+/*-
+ * Copyright (c) 2023 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef bootinfo_h_included
+#define	bootinfo_h_included
+
+/*
+ * Linux/m68k boot information structures.  These records are used to
+ * pass information from the Linux boot loader (or Qemu) to the loaded
+ * kernel.
+ *
+ * Each record has a header with the type tag and record size, followed
+ * by a data field.  Each record + data is padded to align the record
+ * header at a 4-byte boundary.
+ */
+
+struct bi_record {
+	uint16_t	bi_tag;		/* record type */
+	uint16_t	bi_size;	/* record size (including header) */
+	uint8_t		bi_data[];
+};
+
+/*
+ * Record types.  Record types >= 0x8000 are machine-dependent.
+ */
+#define	BI_MACHDEP(x)	((x) + 0x8000)
+#define	BI_LAST		0	/* list terminator */
+#define	BI_MACHTYPE	1	/* machine type (uint32_t) */
+#define	BI_CPUTYPE	2	/* CPU type (uint32_t) */
+#define	BI_FPUTYPE	3	/* FPU type (uint32_t) */
+#define	BI_MMUTYPE	4	/* MMU type (uint32_t) */
+#define	BI_MEMCHUNK	5	/* memory segment (bi_mem_info) */
+#define	BI_RAMDISK	6	/* RAM disk (bi_mem_info) */
+#define	BI_COMMAND_LINE	7	/* kernel command line parameters (C string) */
+#define	BI_RNG_SEED	8	/* random number generator seed (bi_data) */
+
+#define	BI_VIRT_QEMU_VERSION	BI_MACHDEP(0)	/* uint32_t */
+#define	BI_VIRT_GF_PIC_BASE	BI_MACHDEP(1)	/* bi_virt_dev */
+#define	BI_VIRT_GF_RTC_BASE	BI_MACHDEP(2)	/* uint32_t */
+#define	BI_VIRT_GF_TTY_BASE	BI_MACHDEP(3)	/* uint32_t */
+#define	BI_VIRT_VIRTIO_BASE	BI_MACHDEP(4)	/* uint32_t */
+#define	BI_VIRT_CTRL_BASE	BI_MACHDEP(5)	/* uint32_t */
+
+/*
+ * pg68k bootinfo record types.
+ *
+ * BI_PG68K_PLATFORM		bi_data (C string list)
+ * This is a copy of the "compatible" property from the root node of
+ * the device tree.  This is intended to enable early bootstrap code
+ * to make platform-specific decisions before the FDT can be parsed.
+ *
+ * BI_PG68K_FDT			bi_data
+ * Flattened Device Tree describing the system.
+ *
+ * BI_PG68K_ELF_SYMS		bi_mem_info
+ * Symbol table for the loaded program.  Symbol table is in the format
+ * of a standard ELF image.  The loaded program may assume that the
+ * symbols are contiguous with the program image and trailing bootinfo
+ * data.
+ */
+#define	BI_PG68K_PLATFORM	BI_MACHDEP(0)	/* bi_data (C string list) */
+#define	BI_PG68K_FDT		BI_MACHDEP(1)	/* bi_mem_info */
+#define	BI_PG68K_ELF_SYMS	BI_MACHDEP(2)	/* bi_mem_info */
+
+struct bi_mem_info {
+	uint32_t	mem_addr;	/* PA of memory segment */
+	uint32_t	mem_size;	/* size in bytes */
+};
+
+struct bi_data {
+	uint16_t	data_length;	/* length of data */
+	uint8_t		data_bytes[];
+};
+
+struct bi_virt_dev {
+	uint32_t	vd_mmio_base;
+	uint32_t	vd_irq_base;
+};
+
+/*
+ * Values for BI_MACHTYPE.
+ */
+#define	BI_MACH_AMIGA		1
+#define	BI_MACH_ATARI		2
+#define	BI_MACH_MAC		3
+#define	BI_MACH_APOLLO		4
+#define	BI_MACH_SUN3		5
+#define	BI_MACH_MVME147		6
+#define	BI_MACH_MVME16x		7
+#define	BI_MACH_BVME6000	8
+#define	BI_MACH_HP300		9
+#define	BI_MACH_Q40		10
+#define	BI_MACH_SUN3X		11
+#define	BI_MACH_M54XX		12
+#define	BI_MACH_M5441X		13
+#define	BI_MACH_VIRT		14
+/* pg68k extensions: */
+#define	BI_MACH_PG68K		-1
+
+/*
+ * Values for BI_CPUTYPE.
+ */
+#define	BI_CPU_68020		__BIT(0)
+#define	BI_CPU_68030		__BIT(1)
+#define	BI_CPU_68040		__BIT(2)
+#define	BI_CPU_68060		__BIT(3)
+#define	BI_CPU_COLDFIRE		__BIT(4)
+/* pg68k extensions: */
+#define	BI_CPU_68010		__BIT(31)
+
+/*
+ * Values for BI_FPUTYPE.
+ */
+#define	BI_FPU_68881		__BIT(0)
+#define	BI_FPU_68882		__BIT(1)
+#define	BI_FPU_68040		__BIT(2)
+#define	BI_FPU_68060		__BIT(3)
+#define	BI_FPU_SUNFPA		__BIT(4)
+#define	BI_FPU_COLDFIRE		__BIT(5)
+
+/*
+ * Values for BI_MMUTYPE.
+ */
+#define	BI_MMU_68851		__BIT(0)
+#define	BI_MMU_68030		__BIT(1)
+#define	BI_MMU_68040		__BIT(2)
+#define	BI_MMU_68060		__BIT(3)
+#define	BI_MMU_APOLLO		__BIT(4)
+#define	BI_MMU_SUN3		__BIT(5)
+#define	BI_MMU_COLDFIRE		__BIT(6)
+/* pg68k extensions: */
+#define	BI_MMU_PG010		__BIT(31)
+
+#define	bootinfo_dataptr(bi)	((void *)&(bi)->bi_data[0])
+
+struct bi_record *	bootinfo_set_u32(struct bi_record *, uint16_t,
+					 uint32_t);
+struct bi_record *	bootinfo_set_mem_info(struct bi_record *, uint16_t,
+					      uint32_t, uint32_t);
+struct bi_record *	bootinfo_set_data(struct bi_record *, uint16_t,
+					  const void *, size_t);
+struct bi_record *	bootinfo_set_string(struct bi_record *, uint16_t,
+					    const char *);
+
+struct bi_record *	bootinfo_next(struct bi_record *);
+struct bi_record *	bootinfo_find(void *, uint32_t);
+void			bootinfo_enumerate(void *,
+					   bool (*)(struct bi_record *, void *),
+					   void *);
+size_t			bootinfo_size(void);
+void			bootinfo_populate(void *);
+
+#endif /* bootinfo_h_included */
